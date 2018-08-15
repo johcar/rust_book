ec979179b6c55fbfdf0734ff63ffeb0401e77d1e
Johan Carlström <johancarlstrom@gmail.com>
2018-08-15 15:21:35 +0200

    added propagate_errors

---
+++ ch0902_propagate_errors/src/main.rs
@@ -0,0 +1,42 @@
+
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
+// returning the result so that calling code can decide what to do
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// the ? means to return the error to caller, removing alot of the boilerplate from the example above
+fn read_username_from_file_simplified() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+// chaining method calls to simplify even further
+fn read_username_from_file_simplest() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+fn main() {
+
+}

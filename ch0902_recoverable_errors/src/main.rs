e374b917c0628fbe2a3d32955c2d52624141182c
Johan Carlström <johancarlstrom@gmail.com>
2018-08-15 15:21:18 +0200

    added recoverable_errors

---
+++ ch0902_recoverable_errors/src/main.rs
@@ -0,0 +1,28 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let f = File::open("hello.txt");
+
+    // this works but can be a bit verbose, see expect and unwarp example for "cleaner" handling
+    let f = match f {
+        Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!(
+                        "Tried to create file but there was a problem: {:?}",
+                        e
+                    )
+                },
+            }
+        },
+        Err(error) => {
+            panic!(
+                "There was a problem opening the file: {:?}",
+                error
+            )
+        },
+    };
+}

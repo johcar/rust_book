ee9883ca4025b1a689828e8c326cdbf9ea5b1604
Johan Carlström <johancarlstrom@gmail.com>
2018-08-20 12:16:36 +0200

    now returning reference to largest value instead of the largest value itself

--- ch1002_generics_traits/src/main.rs
+++ ch1002_generics_traits/src/main.rs
@@ -1,13 +1,41 @@
-fn largest<T: PartialOrd + Copy>(list: &[T]) -> &T {
-    let mut largest = list[0];
+// 10-15 from book
+//fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+//   let mut largest = list[0];
+//
+//    for &item in list.iter() {
+//        if item > largest {
+//            largest = item;
+//        }
+//    }
+//
+//   largest
+//}
 
-    for &item in list.iter() {
+//fn main() {
+//    let number_list = vec![34, 50, 25, 100, 65];
+//
+//   let result = largest(&number_list);
+//    println!("The largest number is {}", result);
+//
+//    let char_list = vec!['y', 'm', 'a', 'q'];
+//
+//    let result = largest(&char_list);
+//    println!("The largest char is {}", result);
+//}
+
+
+// this version return a reference to the largest value instead of a copy as in listing 10-15 above
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> &T {
+    let mut largest_index = 0;
+    let mut largest = list[0];
+    for (index, &item) in list.iter().enumerate() {
         if item > largest {
+            largest_index = index;
             largest = item;
         }
     }
 
-    largest
+    &list[largest_index]
 }
 
 fn main() {

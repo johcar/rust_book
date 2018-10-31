b33a6f6b4a88b32b4d99085625301f0a33e48ccd
johcar <johancarlstrom@gmail.com>
2018-11-01 00:57:04 +0100

    pulled Config into scope

--- ch1200_minigrep/src/main.rs
+++ ch1200_minigrep/src/main.rs
@@ -2,11 +2,12 @@ extern crate minigrep;
 
 use std::env;
 use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
